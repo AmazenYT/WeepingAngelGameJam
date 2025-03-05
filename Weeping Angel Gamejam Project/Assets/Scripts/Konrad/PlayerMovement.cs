814e01d158973f49325c663a0d10bdabeb29e8ad
Michael4Camps <C24354791@myTUDublin.ie>
2025-03-05 15:14:21 +0000

    Audio(Sprinting)

--- Weeping Angel Gamejam Project/Assets/Scripts/Konrad/PlayerMovement.cs
+++ Weeping Angel Gamejam Project/Assets/Scripts/Konrad/PlayerMovement.cs
@@ -26,6 +26,7 @@ public class PlayerMovement : MonoBehaviour
 
 	//For Footsteps//
 	public AudioSource footstepsSound;
+	public AudioSource sprintSound;
 	public KeyCode w = KeyCode.W;
     public KeyCode a = KeyCode.A;
     public KeyCode s = KeyCode.S;
@@ -79,13 +80,20 @@ public class PlayerMovement : MonoBehaviour
 	{
 		MovePlayer();
 
+		//For Walking and Sprinting Audio//
 		if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D))
 		{
 			footstepsSound.enabled = true;
+			if (Input.GetKey(KeyCode.LeftShift))
+			{
+                footstepsSound.enabled = false;
+				sprintSound.enabled = true;
+            }
 		}
 		else
 		{
 			footstepsSound.enabled = false;
+			sprintSound.enabled = false;
 		}
 
 	}
